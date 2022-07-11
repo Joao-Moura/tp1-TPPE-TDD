14f3e7c93ab1ac636c7cb822984a161d577bd342
Joao-Moura <jaummoura15@gmail.com>
2022-07-11 15:27:23 -0300

    Triangulando teste fração 15 minutos
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- src/tests/test_fracao_quinze.py
+++ src/tests/test_fracao_quinze.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 import pytest
 from unittest import TestCase
+from parameterized import parameterized
 
 from classes.estacionamento import Estacionamento
 
@@ -11,14 +12,12 @@ class TestFacaoQuinze(TestCase):
     def setUp(self):
         self.estacionamento = Estacionamento(valor_fracao=30)
 
+    @parameterized.expand([
+        (timedelta(hours=8, minutes=30), timedelta(hours=8, minutes=56), 60),
+        (timedelta(hours=18, minutes=40), timedelta(hours=19, minutes=20), 90),
+        (timedelta(hours=10, minutes=15), timedelta(hours=10, minutes=30), 30)
+    ])
     @pytest.mark.TesteFuncional
-    def test_cadastro_fracao_um(self):
-        actual = self.estacionamento.calcula_preco(
-            timedelta(hours=8, minutes=30), timedelta(hours=8, minutes=56))
-        self.assertEqual(actual, 60)
-
-    @pytest.mark.TesteFuncional
-    def test_cadastro_fracao_dois(self):
-        actual = self.estacionamento.calcula_preco(
-            timedelta(hours=18, minutes=40), timedelta(hours=19, minutes=20))
-        self.assertEqual(actual, 90)
+    def test_cadastro_fracao(self, hora_entrada, hora_saida, preco_total):
+        actual = self.estacionamento.calcula_preco(hora_entrada, hora_saida)
+        self.assertEqual(actual, preco_total)
