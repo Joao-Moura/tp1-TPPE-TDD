fc999ae96753b26b0b7906f0efc9a099ae12bea5
Joao-Moura <jaummoura15@gmail.com>
2022-07-11 15:49:46 -0300

    Triangulando teste hora cheia
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- src/classes/estacionamento.py
+++ src/classes/estacionamento.py
@@ -5,11 +5,15 @@ class Estacionamento:
     def __init__(self, valor_fracao=None, desconto_hora_cheia=None):
         self.valor_fracao = valor_fracao
         self.desconto_hora_cheia = desconto_hora_cheia
+        
+    @property
+    def hora_cheia_descontada(self):
+        return self.valor_fracao * 4 * ((100 - self.desconto_hora_cheia) / 100)
 
     def calcula_preco(self, hora_inicial, hora_final):
         fracoes = ceil((hora_final - hora_inicial).seconds / (60 * 15))
 
         if fracoes >= 4:
-            return self.valor_fracao * fracoes * ((100 - self.desconto_hora_cheia) / 100)
+            return ((fracoes // 4) * self.hora_cheia_descontada) + (fracoes % 4) * self.valor_fracao
 
         return fracoes * self.valor_fracao
--- src/tests/test_fracao_quinze.py
+++ src/tests/test_fracao_quinze.py
@@ -30,12 +30,12 @@ class TestHoraCheia(TestCase):
             valor_fracao=30, desconto_hora_cheia=15
         )
 
+    @parameterized.expand([
+        (timedelta(hours=8, minutes=30), timedelta(hours=9, minutes=30), 102),
+        (timedelta(hours=8, minutes=30), timedelta(hours=10, minutes=30), 204),
+        (timedelta(hours=10, minutes=15), timedelta(hours=12, minutes=30), 234)
+    ])
     @pytest.mark.TesteFuncional
-    def test_cadastra_hora_cheia_um(self):
-        actual = self.estacionamento.calcula_preco(timedelta(hours=8, minutes=30), timedelta(hours=9, minutes=30))
-        self.assertEqual(actual, 102)
-
-    @pytest.mark.TesteFuncional
-    def test_cadastra_hora_cheia_dois(self):
-        actual = self.estacionamento.calcula_preco(timedelta(hours=8, minutes=30), timedelta(hours=10, minutes=30))
-        self.assertEqual(actual, 204)
+    def test_cadastra_hora_cheia(self, hora_entrada, hora_saida, preco_total):
+        actual = self.estacionamento.calcula_preco(hora_entrada, hora_saida)
+        self.assertEqual(actual, preco_total)
