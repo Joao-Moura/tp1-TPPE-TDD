21743c796d6d7ce38e71aeb7d9f634f6f93ee1f3
Joao-Moura <jaummoura15@gmail.com>
2022-07-11 15:21:24 -0300

    Duplicação teste fração 15 minutos
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- src/classes/estacionamento.py
+++ src/classes/estacionamento.py
@@ -1,6 +1,9 @@
+from math import ceil
+
+
 class Estacionamento:
     def __init__(self, valor_fracao):
         self.valor_fracao = valor_fracao
 
     def calcula_preco(self, hora_inicial, hora_final):
-        return 60
+        return ceil((hora_final - hora_inicial).seconds / (60 * 15)) * self.valor_fracao
--- src/tests/test_fracao_quinze.py
+++ src/tests/test_fracao_quinze.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import timedelta
 
 import pytest
 from unittest import TestCase
@@ -12,7 +12,13 @@ class TestFacaoQuinze(TestCase):
         self.estacionamento = Estacionamento(valor_fracao=30)
 
     @pytest.mark.TesteFuncional
-    def test_cadastro_uma_fracao(self):
+    def test_cadastro_fracao_um(self):
         actual = self.estacionamento.calcula_preco(
-            time.fromisoformat('08:30'), time.fromisoformat('08:56'))
+            timedelta(hours=8, minutes=30), timedelta(hours=8, minutes=56))
         self.assertEqual(actual, 60)
+
+    @pytest.mark.TesteFuncional
+    def test_cadastro_fracao_dois(self):
+        actual = self.estacionamento.calcula_preco(
+            timedelta(hours=18, minutes=40), timedelta(hours=19, minutes=20))
+        self.assertEqual(actual, 90)
