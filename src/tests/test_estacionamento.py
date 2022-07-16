7b2dbd2f683d45dfc9910c1ed266a0d54a9287f7
Joao-Moura <jaummoura15@gmail.com>
2022-07-16 15:17:33 -0300

    Implementando as factorys em todos os testes

--- src/tests/test_estacionamento.py
+++ src/tests/test_estacionamento.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 from parameterized import parameterized
 
 from classes.estacionamento import Estacionamento
+from excecoes.excecao import *
 from tests.factory import EstacionamentoFactory
 
 
@@ -12,6 +13,7 @@ class TestEstacionamentoMixin:
     def defaultSetUp(self):
         self.estacionamento = Estacionamento(
             porcentagem_contratante=50,
+            capacidade=10,
             valor_fracao=30,
             desconto_hora_cheia=15,
             diaria_diurna=120,
@@ -21,6 +23,7 @@ class TestEstacionamentoMixin:
             valor_mensal=600,
             valor_evento=50
         )
+        self.factory = EstacionamentoFactory(self.estacionamento)
 
 
 class TestFracaoQuinze(TestCase, TestEstacionamentoMixin):
@@ -29,13 +32,13 @@ class TestFracaoQuinze(TestCase, TestEstacionamentoMixin):
         self.defaultSetUp()
 
     @parameterized.expand([
-        (timedelta(hours=8, minutes=30), timedelta(hours=8, minutes=56), 60),
-        (timedelta(hours=18, minutes=40), timedelta(hours=19, minutes=20), 90),
-        (timedelta(hours=10, minutes=15), timedelta(hours=10, minutes=30), 30)
+        ('08:30', '08:56', 60),
+        ('18:40', '19:20', 90),
+        ('10:15', '10:30', 30)
     ])
     @pytest.mark.TesteFuncional
     def test_cadastro_fracao(self, hora_entrada, hora_saida, preco_total):
-        actual = self.estacionamento.calcula_preco(hora_entrada, hora_saida)
+        actual = self.factory.cria_por_hora(hora_entrada, hora_saida)
         self.assertEqual(actual, preco_total)
 
 
@@ -45,13 +48,13 @@ class TestHoraCheia(TestCase, TestEstacionamentoMixin):
         self.defaultSetUp()
 
     @parameterized.expand([
-        (timedelta(hours=8, minutes=30), timedelta(hours=9, minutes=30), 102),
-        (timedelta(hours=8, minutes=30), timedelta(hours=10, minutes=30), 204),
-        (timedelta(hours=10, minutes=15), timedelta(hours=12, minutes=30), 234)
+        ('08:30', '9:30', 102),
+        ('08:30', '10:30', 204),
+        ('10:15', '12:30', 234)
     ])
     @pytest.mark.TesteFuncional
     def test_cadastra_hora_cheia(self, hora_entrada, hora_saida, preco_total):
-        actual = self.estacionamento.calcula_preco(hora_entrada, hora_saida)
+        actual = self.factory.cria_por_hora(hora_entrada, hora_saida)
         self.assertEqual(actual, preco_total)
 
 
@@ -61,13 +64,13 @@ class TestDiariaDiurna(TestCase, TestEstacionamentoMixin):
         self.defaultSetUp()
 
     @parameterized.expand([
-        (timedelta(hours=8, minutes=30), timedelta(hours=18, minutes=30)),
-        (timedelta(hours=6), timedelta(hours=15, minutes=1)),
-        (timedelta(hours=9, minutes=15), timedelta(hours=19, minutes=30))
+        ('08:30', '18:30'),
+        ('06:00', '15:01'),
+        ('09:15', '19:30')
     ])
     @pytest.mark.TesteFuncional
     def test_diaria_diurna(self, hora_entrada, hora_saida):
-        actual = self.estacionamento.calcula_preco(hora_entrada, hora_saida)
+        actual = self.factory.cria_por_hora(hora_entrada, hora_saida)
         self.assertEqual(actual, 120)
 
 
@@ -77,13 +80,13 @@ class TestDiariaNoturna(TestCase, TestEstacionamentoMixin):
         self.defaultSetUp()
     
     @parameterized.expand([
-        (timedelta(hours=20), timedelta(hours=7)),
-        (timedelta(hours=19, seconds=1), timedelta(hours=7, minutes=59, seconds=59)),
-        (timedelta(hours=23, minutes=15), timedelta(hours=23, minutes=30))
+        ('20:00', '07:00'),
+        ('19:01', '7:59'),
+        ('23:15', '23:30')
     ])
     @pytest.mark.TesteFuncional
-    def test_diaria_noturna_um(self, hora_entrada, hora_saida):
-        actual = self.estacionamento.calcula_preco(hora_entrada, hora_saida)
+    def test_diaria_noturna(self, hora_entrada, hora_saida):
+        actual = self.factory.cria_por_hora(hora_entrada, hora_saida)
         self.assertEqual(actual, 54)
 
 
@@ -98,7 +101,7 @@ class TestTipoAcesso(TestCase, TestEstacionamentoMixin):
     ])
     @pytest.mark.TesteFuncional
     def test_tipo_acesso(self, tipo_acesso, preco_total):
-        actual = self.estacionamento.calcula_preco(tipo_acesso=tipo_acesso)
+        actual = self.factory.cria_por_acesso(tipo_acesso)
         self.assertEqual(actual, preco_total)
 
 
@@ -106,7 +109,6 @@ class TestValorContratante(TestCase, TestEstacionamentoMixin):
 
     def setUp(self):
         self.defaultSetUp()
-        self.factory = EstacionamentoFactory(self.estacionamento)
 
     @parameterized.expand([
         ([('acesso', "Evento"), ('hora', '08:30', '18:30'), ('hora', '08:30', '09:10')], 130),
@@ -114,7 +116,7 @@ class TestValorContratante(TestCase, TestEstacionamentoMixin):
         ([('acesso', "Mensalista"), ('acesso', "Evento")], 325)
     ])
     @pytest.mark.TesteFuncional
-    def test_calcula_retorno_com_multiplos_acessos(self, acessos, expected):
-        for acesso in acessos:
-            getattr(self.factory, f'cria_por_{acesso[0]}')(*acesso[1:])
+    def test_calcula_retorno_com_multiplos_acessos(self, entradas, expected):
+        for entrada in entradas:
+            getattr(self.factory, f'cria_por_{entrada[0]}')(*entrada[1:])
         self.assertEqual(expected, self.estacionamento.retorno_contratante)
